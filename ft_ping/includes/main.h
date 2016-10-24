3ec36a0b87baabe64283780023c76db73446d3a0
Mhedi BOUGRINE <mbougrin@e3r3p16.42.fr>
2016-10-24 11:13:10 +0200

    add

--- ft_ping/includes/main.h
+++ ft_ping/includes/main.h
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 11:02:55 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/10/24 11:10:22 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/10/24 11:13:09 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,11 @@
 # define PACKET_SIZE 64
 # define NUMBER_PACKET 20
 # define WAIT 2
+
+typedef struct			s_packet
+{
+	icmphdr				hdr;
+	char				msg[PACKET_SIZE - sizeof(icmphdr)];
+}						t_packet;
 
 #endif
