86b8810e2547fb48713c70fd67453b89cc1aae88
Mhedi BOUGRINE <mbougrin@e3r3p16.42.fr>
2016-10-24 11:10:38 +0200

    add

--- ft_ping/includes/main.h
+++ ft_ping/includes/main.h
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 11:02:55 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/10/18 11:05:22 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/10/24 11:10:22 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,21 @@
 
 # include <libft.h>
 # include <stdio.h>
+
+# include <errno.h>
+# include <time.h>
+
+# include <sys/socket.h>
+# include <sys/wait.h>
+
+# include <fcntl.h>
+# include <resolv.h>
+# include <netdb.h>
+# include <netinet/in.h>
+# include <netinet/ip_icmp.h>
+
+# define PACKET_SIZE 64
+# define NUMBER_PACKET 20
+# define WAIT 2
 
 #endif
--- ft_ping/src/main.c
+++ ft_ping/src/main.c
@@ -6,13 +6,30 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 11:02:44 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/10/18 11:11:59 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/10/18 15:02:49 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <main.h>
 
-int				main(int ac, char **av)
+static unsigned short	checksum(void *b, int len)
+{
+	unsigned short 	*buf = b;
+	unsigned int 	sum = 0;
+	unsigned short 	result = 0;
+
+	for (sum = 0; len > 1; len -= 2)
+		sum += *buf++;
+	if (len == 1)
+		sum += *(unsigned char*)buf;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	result = ~sum;
+	return (result);
+}
+
+
+int						main(int ac, char **av)
 {
 	if (ac != 2)
 	{
