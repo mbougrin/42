10f10ff0de26954f4bab96b009418691313a8cf9
Mhedi BOUGRINE <mbougrin@e1r9p16.42.fr>
2016-10-25 12:52:08 +0200

    add ft_ping beta

--- ft_ping/src/main.c
+++ ft_ping/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 11:02:44 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/10/25 12:50:21 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/10/25 12:52:07 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -169,10 +169,10 @@ void					ping(t_addrinfo *addr_info)
 		clock_gettime(CLOCK_MONOTONIC, &tstart);
 
 
-//		struct timeval timeout;
-//		tv.tv_sec = WAIT;
-//		tv.tv_usec = 0;
-		setsockopt(sd, SOL_SOCKET, SO_SNDTIMEO, NULL, NULL);
+		struct timeval timeout;
+		tv.tv_sec = WAIT;
+		tv.tv_usec = 0;
+		setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval));
 
 
 		if (recvfrom(sd, &packet, sizeof(packet), 0, (t_sockaddr*)&r_addr, (socklen_t *)&len) > 0 )
