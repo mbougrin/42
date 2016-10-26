7a8c630b850d96c484a14258f5fae676389babcc
Mhedi BOUGRINE <mbougrin@e3r2p18.42.fr>
2016-10-26 12:09:12 +0200

    add ft_ping beta

--- ft_ping/src/main.c
+++ ft_ping/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/18 11:02:44 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/10/26 12:08:48 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/10/26 12:09:09 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -219,7 +219,7 @@ void					printSigint(void)
 
 	ret = 100;
 	one = stc->count / 100;
-	while (ret != 0)
+	while (ret < 1)
 	{
 		tmp = ret * one;
 		printf("%f tmp %d ret %f one", tmp, ret, one);
