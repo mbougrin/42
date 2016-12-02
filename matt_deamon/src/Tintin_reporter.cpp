02ae740754f61528b23c2dc2827e22908b7b2548
Mhedi BOUGRINE <mbougrin@e3r2p13.42.fr>
2016-12-02 11:14:56 +0100

    add

--- matt_deamon/src/Tintin_reporter.cpp
+++ matt_deamon/src/Tintin_reporter.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 09:10:15 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/02 11:13:59 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/02 11:14:55 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,12 @@ Tintin_reporter::Tintin_reporter(void)
 	writelog("INFO", "Started.");
 //	fs.open(str, fstream::out);
 //	fs.close();
+	if ((_fd = open(str, O_WRONLY | O_CREAT, 0666)) == -1)
+	{
+		std::cout << "file is locked" << std::endl;
+		exit(-1);
+	}
+	close(_fd);
 	if ((_fd = open(str, O_WRONLY)) == -1)
 	{
 		std::cout << "file is locked" << std::endl;
