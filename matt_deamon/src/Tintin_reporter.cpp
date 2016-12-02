7de1728318f15a0da943c4d1542b48ffa2b52462
Mhedi BOUGRINE <mbougrin@e3r2p13.42.fr>
2016-12-02 12:59:15 +0100

    add

--- matt_deamon/src/Tintin_reporter.cpp
+++ matt_deamon/src/Tintin_reporter.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 09:10:15 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/02 12:04:26 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/02 12:59:11 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,14 @@ Tintin_reporter::Tintin_reporter(void)
 	{
 		std::cout << "open error" << std::endl;
 		exit(-1);
+	}	
+	close(_fd);
+	if ((_fd = open(str, O_RDONLY)) == -1)
+	{
+		std::cout << "open error" << std::endl;
+		exit(-1);
 	}
+
 	if (is_locked_socket(_fd) != 0)
 	{
 		std::cout << "file is locked" << std::endl;
