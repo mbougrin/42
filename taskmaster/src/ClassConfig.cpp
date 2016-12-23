25ba0b02197a76f1f5266cf1dea5b9c7f3221300
Mhedi BOUGRINE <mbougrin@e3r3p21.42.fr>
2016-12-23 17:01:09 +0100

    add taskmaster parseur

--- taskmaster/src/ClassConfig.cpp
+++ taskmaster/src/ClassConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 11:41:08 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/23 17:00:26 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/23 17:01:07 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void			ClassConfig::init(char *conf, Tintin_reporter log)
 					<< (*i)->getWorkingdir() << "*"
 //					<< i->getAutostart()
 //					<< i->getAutorestart()
-//					<< (*i)->getStartretry() << "*"
+					<< (*i)->getStartretry() << "*"
 //					<< (*i)->getStarttime() << "*"
 //					<< (*i)->getStoptime() << "*"
 //					<< (*i)->getStopsignal() << "*"
