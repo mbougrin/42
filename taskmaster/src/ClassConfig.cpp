eef64fe4b66820a35f79d0725852161cf0e3e626
Mhedi BOUGRINE <mbougrin@e3r3p21.42.fr>
2016-12-23 11:15:23 +0100

    parsing conf taskmaster

--- taskmaster/src/ClassConfig.cpp
+++ taskmaster/src/ClassConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 11:41:08 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/23 11:11:07 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/23 11:15:18 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void			ClassConfig::init(char *conf, Tintin_reporter log)
 			string *name = new string(i->c_str());
 			name->erase(0, 1);
 			name->erase(name->length() - 1, 1);
-			std::cout << strtok(name->c_str(), ":") << std::endl;
+			std::cout << name->find(":") << std::endl;
 //			for (i = _lstconf.begin(); i != _lstconf.end(); ++i)
 //			{
 //				
