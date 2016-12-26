c170a74a7844c0c1df7d443017d1f2e8aa60cc24
Mhedi BOUGRINE <mbougrin@e3r3p21.42.fr>
2016-12-26 18:38:59 +0100

    add parser taskmaster

--- taskmaster/src/ClassConfig.cpp
+++ taskmaster/src/ClassConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 11:41:08 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/26 18:38:21 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/26 18:38:58 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,7 +103,7 @@ void			ClassConfig::run(void)
 
 			strcpy(str[0], "/bin/ls");
 			strcpy(str[1], "-l");
-			strcpy(str[2], NULL);
+			str[2] = NULL;
 
 			execve((*i)->getCmd().c_str(), str, NULL);
 			std::cout << "ok" << std::endl;
