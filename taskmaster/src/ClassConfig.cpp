6a2b7b6b35d0308c4c243930e0de9f25b2496459
Mhedi BOUGRINE <mbougrin@e3r3p21.42.fr>
2016-12-26 20:27:23 +0100

    add parser taskmaster

--- taskmaster/src/ClassConfig.cpp
+++ taskmaster/src/ClassConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 11:41:08 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/26 20:26:27 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/26 20:27:20 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ void			ClassConfig::run(void)
 				sleep((*i)->getStarttime());
 				if ((pid = fork()) < 0)
 					exit(-1);
-				else
+				if (pid > 0)
 				{
 		//		if (pid == 0)
 		//		{
