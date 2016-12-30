4c523bc996621142ea5d4c3e4bd20200da036eb4
Mhedi BOUGRINE <mbougrin@e2r6p1.42.fr>
2016-12-30 11:16:26 +0100

    add taskmaster

--- taskmaster/src/ClassConfig.cpp
+++ taskmaster/src/ClassConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: mbougrin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/09 11:41:08 by mbougrin          #+#    #+#             */
-/*   Updated: 2016/12/30 11:11:30 by mbougrin         ###   ########.fr       */
+/*   Updated: 2016/12/30 11:16:24 by mbougrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -189,8 +189,13 @@ void			ClassConfig::launchbinary(list<ClassProgram*>::iterator i)
 				}
 			}
 			else
-				wait(NULL);
-//				waitpid(pid, &status, WNOWAIT);
+			{
+			//	wait(NULL);
+				int waitstatus;
+		        wait(&waitstatus);
+		        int exitcode = WEXITSTATUS(waitstatus);
+				std::cout << exitcode << " " << (*i)->getName();
+			}
 			check++;
 //			if (ret == -1)
 //				exit(-1);
